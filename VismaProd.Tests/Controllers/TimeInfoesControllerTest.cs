a53851401bfdc685d614672ea9ced0d38d5e9913
Costas Radu <ikikaze07@gmail.com>
2017-07-17 23:59:50 +0300

    Added example Unit Tests

--- VismaProd.Tests/Controllers/TimeInfoesControllerTest.cs
+++ VismaProd.Tests/Controllers/TimeInfoesControllerTest.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
 using System.Web.Mvc;
 using VismaProd.Controllers;
 
 namespace VismaProd.Tests.Controllers
 {
     [TestClass]
-    class TimeInfoesControllerTest
+    public class TimeInfoesControllerTest
     {
         [TestMethod]
         public void Create()
@@ -14,9 +16,33 @@ namespace VismaProd.Tests.Controllers
 
             ViewResult result = controller.Create() as ViewResult;
 
+            Assert.IsNotNull(result);           
+            Assert.AreEqual("Add time to your projects!", result.ViewData["Message"]);
+        }
+
+        [TestMethod]
+        public void Edit()
+        {
+            TimeInfoesController controller = new TimeInfoesController();
+
+            //pass random Guid to controller
+            ViewResult result = controller.Edit(new System.Guid()) as ViewResult;
+            //should return nothing because bad ID
+            Assert.IsNull(result);
+            
+            VismaDB db = new VismaDB();
+
+            //pick random entry
+            Random rand = new Random();
+            int toSkip = rand.Next(1, db.TimeInfoes.Count());
+
+            TimeInfo info = db.TimeInfoes.OrderBy(r => Guid.NewGuid()).Skip(toSkip).Take(1).First();
+
+            result = controller.Edit(info.Id) as ViewResult;
+
             Assert.IsNotNull(result);
-            Assert.IsNotNull(result.Model);
-            Assert.AreEqual("Add time to your projects!", result.ViewBag["Message"]);
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+
         }
     }
 }
